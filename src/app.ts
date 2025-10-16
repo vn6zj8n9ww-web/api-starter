a2fdacb8555a363d8a5c62487f479c9c637e51b2
vn6zj8n9ww-web <travelcomp@travelcomps-MacBook-Air.local>
2025-10-16 15:08:24 -0700

    add logging with helmet, morgan, cors, and cookie parser for logging functionality

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import logger from '#config/logger.ts';
+
 
 const app = express();
 
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser());
+
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }))
+
 app.get('/', (req, res) => {
+  logger.info("Hello from API-Starter");
   res.status(200).send('Hello from Acquisitions!');
 });
 
